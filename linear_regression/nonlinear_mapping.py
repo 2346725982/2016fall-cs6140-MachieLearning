Fix data normalization.

linear_regression/nonlinear_mapping.py
@@ -5,15 +5,32 @@ class NonlinearMapping(object):
     def __init__(self):
         pass
 
-    def convert(self, data, degree=2):
-        sample_size, feature_size = data.shape
-        new_data = np.ones((sample_size, 0))
-        for j in xrange(feature_size):
-            for d in xrange(1, degree + 1):
-                new_values = [[data[i][j] ** d] for i in xrange(sample_size)]
-                new_column = self.normalize(np.array(new_values))
-                new_data = np.hstack((new_data, new_column))
-        return new_data
+    def convert(self, training_data, test_data, degree=2):
+        training_sample_size, feature_size = training_data.shape
+        test_sample_size, feature_size = test_data.shape
+        new_training_data = np.ones((training_sample_size, 0))
+        new_test_data = np.ones((test_sample_size, 0))
+        for j in range(feature_size):
+            for d in range(1, degree + 1):
+                new_training_values = [[training_data[i][j] ** d] for i in xrange(training_sample_size)]
+                new_test_values = [[test_data[i][j] ** d] for i in xrange(test_sample_size)]
+
+                new_training_column = np.array(new_training_values)
+                new_test_column = np.array(new_test_values)
+
+                mean = np.mean(new_training_column)
+                std_deviation = np.std(new_training_column)
+
+                for i in xrange(new_training_column.shape[0]):
+                    new_training_column[i] = (new_training_column[i] - mean) / std_deviation
+
+                for i in xrange(new_test_column.shape[0]):
+                    new_test_column[i] = (new_test_column[i] - mean) / std_deviation
+
+                new_training_data = np.hstack((new_training_data, new_training_column))
+                new_test_data = np.hstack((new_test_data, new_test_column))
+
+        return new_training_data, new_test_data
 
     def normalize(self, column):
         mean = np.mean(column)

linear_regression/ridge_regression.py
@@ -32,7 +32,7 @@ class RidgeRegression(object):
 
     def cross_validation(self, X, y):
         result = collections.defaultdict(dict)
-        for alpha in np.arange(0, 50, 0.1):
+        for alpha in np.arange(0, 100, 0.1):
             split_X = np.split(X, self.k_folds)
             split_y = np.split(y, self.k_folds)
             for i in xrange(self.k_folds):

linear_regression_report.py
@@ -1,5 +1,6 @@
 import scipy.io as sio
 from sklearn import linear_model
+import matplotlib
 from linear_regression import (LinearRegression, NonlinearMapping)
 from report import Report
 
@@ -9,8 +10,9 @@ if __name__ == '__main__':
     mapping = NonlinearMapping()
 
     for n in (1, 2, 5, 10, 20):
-        training_data = mapping.convert(mat_contents['X_trn'], degree=n)
-        test_data = mapping.convert(mat_contents['X_tst'], degree=n)
+        training_data, test_data = mapping.convert(mat_contents['X_trn'],
+                                                   mat_contents['X_tst'],
+                                                   degree=n)
         training_result = mat_contents['Y_trn']
         test_result = mat_contents['Y_tst']
 

logistic_regression/logistic_regression.py
@@ -1,4 +1,5 @@
 import numpy as np
+import scipy
 
 
 class LogisticRegression(object):
@@ -25,7 +26,7 @@ class LogisticRegression(object):
             for i in xrange(len(labels)):
                 self.gradient_descent(X, y, i, labels[i])
 
-    def gradient_descent(self, X, y, i, label, alpha=0.5, epsilon=0.001, max_iter=10000):
+    def gradient_descent(self, X, y, i, label, alpha=1, epsilon=1e-5, max_iter=10000):
         sample_size, feature_size = X.shape
 
         w = np.ones((feature_size, 1))
@@ -34,6 +35,7 @@ class LogisticRegression(object):
         while not coverge and iteration < max_iter:
             loss = self.loss_function(X, y, label, w)
             gradient = np.dot(X.T, loss) / sample_size # + 0.01 * w
+            #  gradient = -X.T.dot(y - scipy.special.expit(X.dot(w)))
             w = w + alpha * gradient
 
             coverge = (np.linalg.norm(gradient) <= epsilon)